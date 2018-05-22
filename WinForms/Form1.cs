a732be01bcee3a1b99e2764093df8601b39e2a05
Dima <Dima@DESKTOP-QVVB42L>
2018-05-22 11:08:08 +0300

    1.0

--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -12,9 +12,33 @@ namespace WinForms
 {
     public partial class Form1 : Form
     {
+
+        int curposX;
+        int curposY;
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            curposX = e.Location.X;
+            curposY = e.Location.Y;
+            button1.Location = new Point(curposX + button1.Size.Width/2, curposY - button1.Size.Height);
+            button2.Location = new Point(curposX - button1.Size.Width, curposY - button1.Size.Height);
+            button3.Location = new Point(curposX - button1.Size.Width, curposY + button1.Size.Height/2);
+            button4.Location = new Point(curposX + button1.Size.Width / 2, curposY + button1.Size.Height/2);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            button1.Location = new Point()
         }
     }
 }
